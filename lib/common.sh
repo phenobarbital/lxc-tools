5abc909a9117c9d749989ce577c8a18eb397eab3
Jesus Lara <jesuslarag@gmail.com>
2013-08-03 15:40:25 -0430

    added basic role functionality

--- lib/common.sh
+++ lib/common.sh
@@ -8,11 +8,67 @@
 
 VERSION='0.2'
 
-function get_version() {
+function get_version() 
+{
 	echo "LXC-tools version $VERSION";
 }
 
-function get_lxcpath() {
+# basic functions
+#
+
+#  If we're running verbosely show a message, otherwise swallow it.
+#
+function message()
+{
+    message="$*"
+
+    if [ ! -z "$VERBOSE" ]; then
+        echo $message >&2;
+    fi
+}
+
+function info()
+{
+	message="$*"
+
+    if [ ! -z "$VERBOSE" ]; then
+        echo -e $message;
+    fi
+}
+
+function error()
+{
+	message="$*"
+	echo -e $message >&2;
+}
+
+optarg_check() 
+{
+    if [ -z "$2" ]; then
+        usage_err "option '$1' requires an argument"
+    fi
+}
+
+function templatedir()
+{
+		if [ -d "/usr/share/lxc-tools/templates.d" ]; then
+			TEMPLATEDIR="/usr/share/lxc-tools/templates.d"
+		else
+			TEMPLATEDIR="./templates.d"
+		fi
+}
+
+function roledir()
+{
+		if [ -d "/usr/share/lxc-tools/role.d" ]; then
+			ROLEDIR="/usr/share/lxc-tools/role.d"
+		else
+			ROLEDIR="./role.d"
+		fi
+}
+
+function get_lxcpath() 
+{
 	if [ -e /etc/lxc/lxc.conf ]; then
 		# using config dir from lxc
 		grep "[ \t]*lxcpath[ \t]*=" /etc/lxc/lxc.conf | awk -F= '{ print $2 }'
@@ -22,7 +78,8 @@ function get_lxcpath() {
 	fi
 }
 
-function get_domain() {
+function get_domain() 
+{
 	_DOMAIN_=`hostname -d`
 	if [ -z "$_DOMAIN_" ]; then
 				echo -n "Domain not defined, please define a domain [ex: example.com] "
@@ -39,7 +96,8 @@ function get_domain() {
 }
 
 # return distribution based on lsb-release
-function get_distribution() {
+function get_distribution() 
+{
 	if [ -z $(which lsb_release) ]; then
 		echo "lxc-tools error: lsb-release is required"
 		exit 1
@@ -48,7 +106,8 @@ function get_distribution() {
 }
 
 # get codename (ex: wheezy)
-function get_suite() {
+function get_suite() 
+{
 	if [ -z $(which lsb_release) ]; then
 		echo "lxc-tools error: lsb-release is required"
 		exit 1
@@ -56,7 +115,8 @@ function get_suite() {
 	lsb_release -s -c
 }
 
-function get_lxcversion() {
+function get_lxcversion() 
+{
 	if [ `get_distribution` = "Debian" ]; then
 		dpkg-query -W -f='${Version}' lxc
 		# TODO: per distribution lxc version
@@ -64,7 +124,8 @@ function get_lxcversion() {
 }
 
 # get architecture
-function get_arch() {
+function get_arch() 
+{
 	if [ -z $(which lsb_release) ]; then
 		echo "lxc-tools error: lsb-release is required"
 		exit 1
@@ -75,7 +136,8 @@ function get_arch() {
 }
 
 # get repository name
-function get_suite_repository() {
+function get_suite_repository() 
+{
 	dist=`get_distribution`
 	if [ "$dist" = "Debian" ]; then
 		suite=`get_suite`
@@ -86,12 +148,14 @@ function get_suite_repository() {
 }
 
 # obtener el nombre del grupo de volumen LVM
-function get_lvm() {
+function get_lvm() 
+{
 	# determinar el nombre del primer volumen lógico
     vgs | tail -n1 | awk '{print $1}'
 }
 
-function get_default_bridge() {
+function get_default_bridge() 
+{
 	if [ "$BRIDGE" = "auto" ]; then
 		# obtenemos la lista de interfaces activas
 		get_bridge
@@ -105,7 +169,8 @@ function get_default_bridge() {
 	fi
 }
 
-function cgroup_enabled() {
+function cgroup_enabled() 
+{
 		cgroup=`grep cgroup /proc/self/mounts`
 		if [ -z "$cgroup" ]; then
 			echo "lxc-tools error: cgroups are not configured"
@@ -118,3 +183,17 @@ function cgroup_enabled() {
 		return 0
 }
 
+# install a package into chroot
+function install_package()
+{
+	# get distribution
+	dist=`get_distribution`
+	if [ "$dist" = "Debian" ]; then
+		message "installing Debian package $@"
+		#
+		# Install the packages
+		#
+		DEBIAN_FRONTEND=noninteractive chroot $ROOTFS /usr/bin/apt-get --yes --force-yes install "$@"
+	fi
+	# TODO: other distro versions
+}
